98eafe6458450e752219e30688c070bda087d420
12q1 <thomasham89@gmail.com>
2018-09-15 12:41:30 +0200

    rest function tests completed

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -9,7 +9,25 @@ const hero = {
   },
 };
 
+function rest(object){
+  let restore = object
+    if (restore.health < 10) {
+      restore.health = 10;
+    }
+    object = restore;
+    return object;
+}
 
+function restoreHealth(){
+    console.log("inn button has been clicked");
+    hero.health = 10;
+  }
 
+function pickUpItem(){
+}
+
+function equipWeapon(){
+
+}
 
 // Write your JS here
--- scraps.js
+++ scraps.js
@@ -8,3 +8,21 @@ const "hero" = {
     damage : 2,
   },
 }
+
+function rest(object){
+    object.heath = 10; //so this doesn't work...
+}
+
+function rest(object){
+    this.heath = 10; //this doesn't work either
+}
+
+function rest(object){
+    object = object.heath;
+    object = 10;
+} //nope
+
+function rest(object){
+    if (object.health<10)
+      object.health = 10
+}
