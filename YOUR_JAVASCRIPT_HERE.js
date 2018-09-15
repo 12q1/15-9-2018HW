c74f07765108a59dd1d9dd35c1793d2a4f3a90a1
12q1 <thomasham89@gmail.com>
2018-09-15 11:10:20 +0200

    first commit

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,13 @@
+var "hero" = {
+  name : "Captain Obvious",
+  hero : true,
+  inventory : [],
+  health : 10,
+  weapon : {
+    type : "sarcasm",
+    damage : 2,
+  },
+}
+
+
+// Write your JS here
